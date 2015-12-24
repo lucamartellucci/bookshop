2b098dbd8e0bfaa73d4bd32645d839f41ed39ff7
luca <luca.martellucci@absontheweb.com>
2015-12-24 08:52:27 +0100

    detached e2e tests from build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -427,8 +427,9 @@ module.exports = function (grunt) {
     'concurrent:test',
     'autoprefixer',
     'connect:test',
-    'karma',
-    'protractor:run'
+    'karma'
+//  ,
+//  'protractor:run'
   ]);
 
   grunt.registerTask('build', [
